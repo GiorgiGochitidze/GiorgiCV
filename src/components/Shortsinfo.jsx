451bde1022ff71716c496b646562b080de3acdaf
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2022-12-16 21:45:52 +0400

    deleted:    dist/assets/index.cb89bbe2.js 	new file:   dist/assets/index.ff725e37.js 	modified:   dist/index.html 	modified:   src/components/Shortsinfo.jsx

--- src/components/Shortsinfo.jsx
+++ src/components/Shortsinfo.jsx
@@ -12,7 +12,7 @@ const Shortsinfo = () => {
                 <div className='info'>
                     <h1>HI, I'm Giorgi</h1>
                     <p style={{marginTop: '-20px'}}>Frontend Developer</p>
-                    <p>High level experience in<br /> Web development knowfedge,<br /> producion quality work.</p>
+                    <p>High level experience in<br /> Web development knowledge,<br /> production quality work.</p>
                     <a style={{textDecoration: 'none'}} href='https://www.linkedin.com/in/giorgi-gochitidze-5a1840251/' target="_blank"><button className='contactme-button'>CONTACT ME <img src={leftarrow} style={{width: '40px', height: '40px'}} /></button></a>
                 </div>
             </div>
